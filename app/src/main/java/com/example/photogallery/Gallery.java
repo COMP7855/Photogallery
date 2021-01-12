af34aac554ced24fa2014eee14b1cad7b0c3743a
Ogre <qyw.henry@gmail.com>
2021-01-11 19:14:38 -0800

    updated

--- app/src/main/java/com/example/photogallery/Gallery.java
+++ app/src/main/java/com/example/photogallery/Gallery.java
@@ -2,7 +2,9 @@ package com.example.photogallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gallery extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Gallery extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
     }
+
+    public void onButtonClick_search(View v) {
+        Intent myIntent = new Intent(getBaseContext(), Search.class);
+        startActivity(myIntent);
+    }
+
 }
