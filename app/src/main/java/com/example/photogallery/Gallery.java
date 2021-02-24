4b931ab4d7092dea02144f14aeb9660f4f978534
JoshPenner <ircobra@gmail.com>
2021-02-24 15:50:06 -0800

    Refactoring for complexity
    Simplified a few functions by creating new methods and reorganizing code. Added comments and updated variable names for clarity.

--- app/src/main/java/com/example/photogallery/Gallery.java
+++ app/src/main/java/com/example/photogallery/Gallery.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.location.Location;
 
 import android.Manifest;
@@ -13,10 +14,8 @@ import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Debug;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.Html;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -30,21 +29,19 @@ import com.google.android.gms.tasks.Task;
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.Locale;
 
 
 public class Gallery extends AppCompatActivity {
     public static final int SEARCH_ACTIVITY_REQUEST_CODE = 10;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String mCurrentPhotoPath;
-    private ArrayList<String> photos = null;
-    private int index = 0;
+    private ArrayList<String> photoPathList = null;
+    private int photoIndex = 0;
     private Date startTimestamp = null;
     private Date endTimestamp = null; //JP
     private String keywords = null; //JP
@@ -58,7 +55,7 @@ public class Gallery extends AppCompatActivity {
     private Double longMin = -999.9;
     private Double longMax = 999.9;
 
-    // upon signing in to photogallery
+    // upon entering gallery view
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -69,12 +66,12 @@ public class Gallery extends AppCompatActivity {
         tvLongitude = (TextView) findViewById(R.id.longitude);
 
         // update list of photos
-        photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
-        if (photos.size() == 0) {
+        photoPathList = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
+        if (photoPathList.size() == 0) {
             displayPhoto(null);
         } else {
             // display the first photo on the list
-            displayPhoto(photos.get(index));
+            displayPhoto(photoPathList.get(photoIndex));
         }
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
@@ -82,37 +79,58 @@ public class Gallery extends AppCompatActivity {
 
     }
 
+    // get last location and write it on the screen
+    @SuppressLint("MissingPermission")
     private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                //Initialize location
+        // if location permission is granted
+        if (checkPermissions())
+        {
+            // get last location
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<Location> task)
+                {
+                    //Initialize location
                     Location location = task.getResult();
-                    if (location != null) {
+                    // set textbox values to current longitude and latitude
+                    if (location != null)
+                    {
                         tvLatitude.setText(String.valueOf(location.getLatitude()));
                         tvLongitude.setText(String.valueOf(location.getLongitude()));
                     }
                 }
-        });
+            });
+
+        }
     }
 
-    //when the "Search" button is pressed
-   // public void onButtonClick_search(View v) {
-       // Intent myIntent = new Intent(getBaseContext(), Search.class);
-       // startActivity(myIntent);
-    //}
+    // check location permissions
+    private boolean checkPermissions()
+    {
+        if (ActivityCompat.checkSelfPermission
+                (Gallery.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        else
+        {
+            requestPermissions();
+            return false;
+        }
+    }
 
-     public void onButtonClick_search(View v) {
+    // request location permissions
+    private void requestPermissions()
+    {
+        ActivityCompat.requestPermissions
+                (Gallery.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        44);
+    }
+
+    // when search button is pressed, open Search Activity
+    public void onButtonClick_search(View v) {
 
      Intent i = new Intent(Gallery.this, Search.class);
 
@@ -120,13 +138,13 @@ public class Gallery extends AppCompatActivity {
 
     }
 
+    // when share button is pressed, share an image
     public void onButtonClick_share(View v) {
-        share_Image(photos.get(index));
+        share_Image(photoPathList.get(photoIndex));
     }
 
+    // share the image at the passed filepath (called from onButtonClick_share)
     public void share_Image(String path) {
-
-
         //ImageView iv = (ImageView) findViewById(R.id.imageViewPic);
         //iv.setImageBitmap(BitmapFactory.decodeFile(path));
         File photoShare = new File(path);
@@ -141,24 +159,26 @@ public class Gallery extends AppCompatActivity {
 
         startActivity(Intent.createChooser(shareIntent, send_to));
 
-
         //File photoShare = photos.get(index);
-
     }
 
-
-    // when the "Snap" button is pressed
+    // when the "Snap" button is pressed, take a picture and store it
     public void onButtonClick_camera(View v) {
+        // create intent for camera
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // if intent successfully created
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // initialize photo file variable
             File photoFile = null;
             try {
+                // create the empty image file in the pictures directory with a temporary name
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
+                // request the camera takes a picture and store it in the photo file
                 Uri photoURI = FileProvider.getUriForFile(this, "com.example.photogallery.fileprovider", photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -188,55 +208,58 @@ public class Gallery extends AppCompatActivity {
                 Double latitude = Double.parseDouble(attr[4]);
                 Double longitude = Double.parseDouble(attr[5]);
 
-                if (((startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime()
-                        && f.lastModified() <= endTimestamp.getTime())
-                ) && (keywords == "" || f.getPath().contains(keywords))
+                if (
+                        (// no timestamp given or
+                        (startTimestamp == null && endTimestamp == null) ||
+                                // within the date range specified
+                                (f.lastModified() >= startTimestamp.getTime() && f.lastModified() <= endTimestamp.getTime())
+                        ) // keywords is empty or the file contains the keywords from search
+                                && (keywords == "" || f.getPath().contains(keywords))
+                                //coordinates are within search area
                         && (latitude >= latMin)
                         && (latitude <= latMax)
                         && (longitude >= longMin)
                         && (longitude <= longMax)
-                )
+                ) // add the photo to the list
                     photos.add(f.getPath());
             }
         }
         return photos;
     }
 
-
-
     // when the "Left" or "Right" buttons are pressed
     public void scrollPhotos(View v) {
         // update current photo's filename using caption text
-        updatePhoto(photos.get(index),
+        updatePhoto(photoPathList.get(photoIndex),
                 ((EditText) findViewById(R.id.editTextCaption)).getText().toString(),
                 tvLatitude.getText().toString(),
                 tvLongitude.getText().toString()
         );
         // update list of photos to have the correct filenames
         if (startTimestamp == null){
-            photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");  // JP
+            photoPathList = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");  // JP
         }
         else {
-            photos = findPhotos(startTimestamp, endTimestamp,keywords);  // JP
+            photoPathList = findPhotos(startTimestamp, endTimestamp,keywords);  // JP
         }
 
         // increase or decrease index depending on button press
         switch (v.getId()) {
             case R.id.buttonLeft:
-                if (index > 0) {
-                    index--;
+                if (photoIndex > 0) {
+                    photoIndex--;
                 }
                 break;
             case R.id.buttonRight:
-                if (index < (photos.size()-1)) {
-                index++;
+                if (photoIndex < (photoPathList.size()-1)) {
+                photoIndex++;
             }
             break;
             default:
                 break;
         }
         // display the picture with the current index value
-        displayPhoto(photos.get(index));
+        displayPhoto(photoPathList.get(photoIndex));
     }
 
     // display the photo
@@ -268,13 +291,13 @@ public class Gallery extends AppCompatActivity {
         }
     }
 
-
-    // creates an image file with a temporary name
+    // returns image file with a temporary name in the pictures directory
     // called from "Snap" button function onButtonClick_camera
     private File createImageFile() throws IOException {
-        // Create an image file name
+        // Create an empty image file name with temporary caption, current time, and location
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "_caption_" + timeStamp + "_000_111_"; // 000 and 111 in place of long and lat
+        // create the image file and store it the image file in the pictures directory
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
@@ -284,65 +307,84 @@ public class Gallery extends AppCompatActivity {
     // when a picture has been taken in the camera app
     // or when a search has been made
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        // search
-        if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                //Date startTimestamp , endTimestamp;
-                try {
-                    String from = (String) data.getStringExtra("STARTTIMESTAMP");
-                    String to = (String) data.getStringExtra("ENDTIMESTAMP");
-                    startTimestamp = format.parse(from);
-                    endTimestamp = format.parse(to);
-                } catch (Exception ex) {
-                    startTimestamp = null;
-                    endTimestamp = null;
-                }
-                keywords = (String) data.getStringExtra("KEYWORDS"); //JP
-
-                //DecimalFormat formatLongLat = new DecimalFormat("0.0000");
-                try {
-                    String strLatMin = (String) data.getStringExtra("LATMIN");
-                    String strLatMax = (String) data.getStringExtra("LATMAX");
-                    String strLongMin = (String) data.getStringExtra("LONGMIN");
-                    String strLongMax = (String) data.getStringExtra("LONGMAX");
-                    latMin = Double.parseDouble(strLatMin);
-                    latMax = Double.parseDouble(strLatMax);
-                    longMin = Double.parseDouble(strLongMin);
-                    longMax = Double.parseDouble(strLongMax);
-                } catch (Exception ex) {
-                    latMin = null;
-                    latMax = null;
-                    longMin = null;
-                    longMax = null;
-                }
-                index = 0;
-                photos = findPhotos(startTimestamp, endTimestamp, keywords);
-                if (photos.size() == 0) {
-                    displayPhoto(null);
-                } else {
-                    displayPhoto(photos.get(index));
-                }
+        // if a search was made
+        if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                displaySearchResults(data);
             }
         }
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //ImageView mImageView = (ImageView) findViewById(R.id.imageViewPic);
-            //mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
-            photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
-            index = photos.size()-1; // JP
-            displayPhoto(photos.get(index)); // JP
+        // if a photo was taken
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            // update list of photos
+            photoPathList = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
+            // display the most recent photo
+            photoIndex = photoPathList.size()-1;
+            displayPhoto(photoPathList.get(photoIndex));
 
-            if (ActivityCompat.checkSelfPermission(Gallery.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                //When permission granted
-                getLocation();
-            } else {
-                ActivityCompat.requestPermissions(Gallery.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-            }
+            // get the current location
+            getLocation();
         }
     }
+
+    // display photos based on the search criteria
+    private void displaySearchResults(Intent data)
+    {
+        // obtain time values from search
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            String from = (String) data.getStringExtra("STARTTIMESTAMP");
+            String to = (String) data.getStringExtra("ENDTIMESTAMP");
+            startTimestamp = format.parse(from);
+            endTimestamp = format.parse(to);
+        } catch (Exception ex) {
+            startTimestamp = null;
+            endTimestamp = null;
+        }
+
+        // obtain keywords from search
+        keywords = (String) data.getStringExtra("KEYWORDS"); //JP
+
+        // obtain location values from search
+        try {
+            String strLatMin = (String) data.getStringExtra("LATMIN");
+            String strLatMax = (String) data.getStringExtra("LATMAX");
+            String strLongMin = (String) data.getStringExtra("LONGMIN");
+            String strLongMax = (String) data.getStringExtra("LONGMAX");
+            latMin = Double.parseDouble(strLatMin);
+            latMax = Double.parseDouble(strLatMax);
+            longMin = Double.parseDouble(strLongMin);
+            longMax = Double.parseDouble(strLongMax);
+        } catch (Exception ex) {
+            latMin = -999.0;
+            latMax = 999.9;
+            longMin = -999.9;
+            longMax = 999.9;
+        }
+
+        // update list of photos based on search values
+        photoPathList = findPhotos(startTimestamp, endTimestamp, keywords);
+        photoIndex = 0;
+
+        // if the list contains photos
+        if (photoPathList.size() > 0)
+        {
+            // display the first photo in the list
+            displayPhoto(photoPathList.get(photoIndex));
+        }
+        // if the list is empty
+        else
+        {
+            // display default image
+            displayPhoto(null);
+        }
+    }
+
 
     // when scrollPhotos runs ("Left or "Right" button pressed)
     // update the currently displayed pictures filename with the textbox caption
@@ -362,5 +404,4 @@ public class Gallery extends AppCompatActivity {
             from.renameTo(to);
         }
     }
-
 }
